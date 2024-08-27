a2dcb4ab16b0bcc8c13d4a9f42de8553a271cd3e
Enrique Stolar <enrique7stolar@gmail.com>
2024-08-27 16:00:21 -0500

    Tasks List

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,24 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Ruta para mostrar el formulario de edición de una tarea
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # Ruta para manejar la actualización de una tarea
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Ruta para mostrar el formulario de creación de una nueva tarea
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # Ruta para manejar la creación de una nueva tarea
+  post 'tasks', to: 'tasks#create'
+
+  # Ruta para listar todas las tareas
+  get 'tasks', to: 'tasks#index'
+
+  # Ruta para mostrar los detalles de una tarea específica
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Ruta para eliminar una tarea
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
